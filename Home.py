40dcf4cf243b4a940dfed933762776c7a6f7293f
prasadmjadhav6959 <122101343+prasadmjadhav6959@users.noreply.github.com>
2023-04-10 10:27:35 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+st.title(':blue[Flipkart Laptop Price Prediction]')
+st.header(':red[Innomatics Research Labs]')
+st.text('Project By PRASAD JADHAV')
